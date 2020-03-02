f7ede0152a3b38eb8e0ab7292d067a662e78b48a
WHALEN Andrew <awhalen@mvm-ri-m155130.roslin.ed.ac.uk>
2020-03-02 13:04:24 +0000

    Updating the build_pipeline to a real pipeline

--- build_pipeline.sh
+++ build_pipeline.sh
@@ -1,6 +1,19 @@
 rm -r build
 rm -r dist
 python setup.py bdist_wheel
-pip uninstall AlphaPeel -y
-pip install dist/AlphaPeel-0.0.1-py3-none-any.whl
+# pip uninstall AlphaPeel -y
+# pip install dist/AlphaPeel-0.0.1-py3-none-any.whl
+
+
+#Compile manual
+ ( cd docs; make latexpdf )
+
+
+target=AlphaPeel
+rm -rf $target
+mkdir $target
+cp dist/* $target
+cp docs/build/latex/AlphaPeel.pdf $target
+cp -r example $target
+zip -r $target.zip $target
 
