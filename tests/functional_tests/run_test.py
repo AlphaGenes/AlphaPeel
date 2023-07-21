4cac2ac2000f901c74c861e2802253cfc6a01862
xinger <s2186420@ed.ac.uk>
2023-07-21 13:49:54 +0100

    Add test 7

--- tests/functional_tests/run_test.py
+++ tests/functional_tests/run_test.py
@@ -4,8 +4,15 @@ import subprocess
 import pytest
 
 
-def read_and_sort_file(file_name, id_list=None, **kwargs):
-    with open(file_name, "r") as file:
+def read_and_sort_file(file_path, id_list=None, **kwargs):
+    """
+    INPUT:
+    file_path: str, the path of the file to be read
+    id_list: None or list of int, the ids of the records to be read
+    OUTPUT:
+    values: 2d list of str, store the sorted values of the (selected) records
+    """
+    with open(file_path, "r") as file:
         values = [line.strip().split() for line in file]
 
     if id_list is not None:
@@ -18,8 +25,11 @@ def read_and_sort_file(file_name, id_list=None, **kwargs):
 
 @pytest.fixture
 def commands_and_paths():
-    commands = []
-    paths = []
+    """
+    Return a dictionary of commands and a dictionary of paths
+    """
+    commands = {}
+    paths = {}
 
     command_1 = "AlphaPeel -genotypes test1/genotypes.txt \
                           -phasefile test1/phasefile.txt \
@@ -42,36 +52,91 @@ def commands_and_paths():
                           -stopsnp 4 \
                           -out test2/outputs/output"
 
+    command_3 = "AlphaPeel -genotypes test3/genotypes.txt \
+                          -phasefile test3/phasefile.txt \
+                          -penetrance test3/penetrance.txt \
+                          -seqfile test3/seqfile.txt \
+                          -pedigree test3/pedigree.txt \
+                          -runType multi \
+                          -calling_threshold .1 0.99\
+                          -startsnp 2 \
+                          -stopsnp 4 \
+                          -binary_call_files \
+                          -out test3/outputs/output"
+
+    command_3b = ""
+
+    command_3c = ""
+
+    command_4 = ""
+
+    command_5 = ""
+
+    command_6 = ""
+
+    command_7 = "AlphaPeel -genotypes test7/genotypes.txt \
+                          -phasefile test7/phasefile.txt \
+                          -penetrance test7/penetrance.txt \
+                          -seqfile test7/seqfile.txt \
+                          -pedigree test7/pedigree.txt \
+                          -runType multi \
+                          -esterrors \
+                          -calling_threshold .1 \
+                          -out test7/outputs/output"
+
+    command_7b = ""
+
+    command_7c = ""
+
+    command_8 = ""
+
     local_variables = locals()
 
-    for variables in local_variables:
-        if variables[:8] == "command_":
-            commands.append(local_variables[variables])
-
     for n in range(1, 9):
-        paths.append(f"test{n}/outputs")
+        test_n = str(n)
+        paths[test_n] = f"test{n}/outputs"
+        commands[test_n] = local_variables["command_" + test_n]
         if n in [3, 7]:
-            paths.append(f"test{n}b/outputs")
-            paths.append(f"test{n}c/outputs")
+            paths[test_n + "b"] = f"test{n}b/outputs"
+            commands[test_n + "b"] = local_variables["command_" + test_n + "b"]
+            paths[test_n + "c"] = f"test{n}c/outputs"
+            commands[test_n + "c"] = local_variables["command_" + test_n + "c"]
 
     return commands, paths
 
 
-def make_direction(path):
+def make_directory(path):
+    """
+    Prepare a empty folder at the input path
+    """
     if os.path.exists(path):
         shutil.rmtree(path)
 
     os.mkdir(path)
 
 
+def delete_columns(two_d_list, col_del):
+    """
+    Delete the columns in col_del of two_d_list
+    """
+    for n in range(len(col_del)):
+        for row in two_d_list:
+            del row[col_del[n] - n - 1]
+
+
 def test_cases(commands_and_paths):
-    for test_number in [1, 2]:
+    """
+    Run the tests
+    """
+    tests = ["1", "2", "7"]
+
+    for test_number in tests:
         command, path = (
-            commands_and_paths[0][test_number - 1],
-            commands_and_paths[1][test_number - 1],
+            commands_and_paths[0][test_number],
+            commands_and_paths[1][test_number],
         )
 
-        make_direction(path)
+        make_directory(path)
 
         subprocess.run(command, shell=True, capture_output=True, text=True)
 
@@ -81,9 +146,11 @@ def test_cases(commands_and_paths):
         output = read_and_sort_file(output_file_path)
         expected = read_and_sort_file(expected_file_path)
 
-        if test_number == 2:
-            for row in expected:
-                del row[1]
-                del row[4]
+        if test_number == "2":
+            delete_columns(expected, [2, 6])
+
+        if test_number == "3":
+            delete_columns(expected, [2, 6])
+            delete_columns(output, [1, 3, 4, 5, 6])
 
         assert output == expected
