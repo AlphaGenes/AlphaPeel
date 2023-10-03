dcd7a1cbb9dd8f10857dc4c35f302376b42a9142
xinger <s2186420@ed.ac.uk>
2023-10-03 15:39:16 +0100

    Revert "Merge branch 'accu_test_file' into devel"
    This reverts commit 62e615fc75012b9a3437c26077b80a38fcd7d46e, reversing
    changes made to 5fd9132631731c75ad5cf93165a955e9afb419a1.

--- tests/accuracy_tests/sim_for_alphapeel_accu_test/sim_for_accu.R
+++ tests/accuracy_tests/sim_for_alphapeel_accu_test/sim_for_accu.R
@@ -1,26 +1,62 @@
-# ----- Setup -----
-
 rm(list = ls())
-# install.packages(pkg = "AlphaSimR")
 library(AlphaSimR)
+library(Rlab)
 
-# ----- Functions -----
+nLoci <- 1000
+nInd <- 1000
+nGen <- 5
+nChr <- 1
+nsegSites <- nLoci/nChr
+genoError <- 0.001
+seqError <- 0.01
+nSelect <- 100
+
+# runMacs for generic species
+founderGenomes <- runMacs(nInd = (nInd / nGen),
+                          nChr = nChr,
+                          segSites = nsegSites,
+                          species = "GENERIC")
+SP <- SimParam$new(founderGenomes)
+SP$setSexes("yes_sys")
+SP$setTrackPed(TRUE)
+SP$setTrackRec(TRUE)
+basePop <- newPop(founderGenomes)
+
+currentPop <- basePop
+Parents <- c()
+for (generation in 1:(nGen - 1)) {
+  # add selections, pick 100 parents randomly, make the parents high density, everyone in gen5 low density
+  newParents <- selectInd(currentPop, nInd = nSelect, use = "rand")
+  Parents <- c(Parents, getPed(newParents)[, 1])
+  newPop <- randCross(currentPop, nCrosses = nInd(currentPop))
+  currentPop <- newPop
+  popList <- list(basePop, currentPop)
+  basePop <- mergePops(popList)
+}
+
+# for the calculation of segregation and the recombination rate
+# recHist[[individual]][[chromosome]][[haplotype]] 
+# column 1 gives the parental haplotype, column 2 gives the start loci
+recHist <- SP$recHist
+IbdHaplo <- pullIbdHaplo(basePop) # flip maternal and paternal haplotype before use
 
 # generate errors in the genotype input
-# geno - matrix, genotypes (ind x loc) coded as 0, 1, 2, or 9 (missing)
-# error - numeric, probability of observing an error (single value or
-#         multiple to add variation across loci)
-generateGenoErr <- function(geno, error) {
-  nLoci <- ncol(geno)
+# locus specific error rate?
+generateGenoErr <- function(geno, error = genoError) {
   for (ind in 1:nInd){
+    error_ind <- rbern(nLoci, error)
     for (locus in 1:nLoci){
-      if (geno[ind, locus] != 9 && rbinom(n = 1, size = 1, prob = error) == 1) {
+      if (error_ind[locus] == 1) {
         if (geno[ind, locus] == 0) {
           geno[ind, locus] <- sample(c(1, 2), size = 1)
-        } else if (geno[ind, locus] == 1) {
-          geno[ind, locus] <- sample(c(0, 2), size = 1)
-        } else {
-          geno[ind, locus] <- sample(c(0, 1), size = 1)
+        }
+        else {
+          if (geno[ind, locus] == 1) {
+            geno[ind, locus] <- sample(c(0, 2), size = 1)
+          }
+          else {
+            geno[ind, locus] <- sample(c(0, 1), size = 1)
+          }
         }
       }
     }
@@ -28,260 +64,185 @@ generateGenoErr <- function(geno, error) {
   return(geno)
 }
 
-# simulateSeqReads
-url <- "https://gist.githubusercontent.com/gregorgorjanc/e7ca7a02ed59242573c12a890cf8c871/raw/e88ad2a3036f3385a0724d9d92a7cf5012ee0f86/simulateSeqReads.R"
-source(file = url)
+# generating genotypes
+genotypes <- pullSegSiteGeno(basePop)
 
-# ----- Simulation parameters -----
-
-nInd <- 1000
-nGen <- 5
-nIndPerGen <- nInd / nGen
-nParents <- 100
-
-nLociAll <- 2000
-nLociHD <- nLociAll
-nLociLD <- 100
-
-nChr <- 1
-nLociAllPerChr <- nLociAll / nChr
-nLociHDPerChr <- nLociHD / nChr
-nLociLDPerChr <- nLociLD / nChr
-
-genoError <- 0.001
-
-seqDepth <- 3
-seqError <- 0.01
-
-# ----- SimParam and base population -----
-
-founderGenomes <- runMacs(nInd = nIndPerGen, nChr = nChr,segSites = nLociAllPerChr)
-SP <- SimParam$new(founderGenomes)
-SP$setSexes("yes_rand")
-SP$setTrackPed(TRUE)
-SP$setTrackRec(TRUE)
-SP$addSnpChip(nSnpPerChr = nLociHDPerChr, name = "HD")
-
-# Create LD chip
-by <- nLociHDPerChr / nLociLDPerChr
-tmp <- seq(from = by / 2, to = nLociHDPerChr, by = by)
-markersLD <- c(outer(X = 1:nChr, Y = tmp, FUN = paste, sep = "_"))
-tmp <- c(outer(X = 1:nChr, Y = 1:nLociHDPerChr, FUN = paste, sep = "_"))
-markersHD_without_LD <- tmp[!tmp %in% markersLD]
-# SP$addSnpChipByName(markers = markersLD, name = "LD") --> we will subset HD later for some individuals
-
-basePop <- newPop(founderGenomes)
-
-# ----- Generate individuals across generations -----
-
-currentPop <- basePop
-allPop <- currentPop
-Parents <- c()
-for (generation in 1:(nGen - 1)) {
-  newParents <- selectInd(currentPop, nInd = nParents, use = "rand")
-  Parents <- c(Parents, newParents@id)
-  currentPop <- randCross(newParents, nCrosses = nIndPerGen)
-  allPop <- c(allPop, currentPop)
-}
-
-# ----- Collect/Generate data& Calculate realised rates -----
-
-# ----- Pedigree -----
-
-pedigree <- getPed(pop = allPop)
-pedigree <- pedigree[, c("id", "father", "mother")]
-
-# ----- Haplotypes ----
-
-haplotypes <- pullSnpHaplo(pop = allPop, snpChip = "HD")
-# flip maternal and paternal haplotype (AlphaSimR puts maternal first, AlphaPeel paternal)
+# generating haplotypes
+haplotypes <- pullSegSiteHaplo(basePop)
+# flip maternal and the paternal haplotype
 for (ind in 1:nInd) {
   maternal <- haplotypes[ind * 2 - 1, ]
   haplotypes[ind * 2 - 1, ] <- haplotypes[ind * 2, ]
   haplotypes[ind * 2, ] <- maternal
 }
 
-# ----- Phased genotypes ------
+url <- "https://gist.githubusercontent.com/gregorgorjanc/e7ca7a02ed59242573c12a890cf8c871/raw/e88ad2a3036f3385a0724d9d92a7cf5012ee0f86/simulateSeqReads.R"
+import::from(pins::pin(url), "simulateSeqReads", .character_only=TRUE)
 
-phasedGenotypes <- matrix(data = 0, nrow = nInd * 4, ncol = nLociAll + 1)
-for (ind in (1:nInd)) {
-  for (locus in (1:nLociAll)) {
-    currentGeno <- haplotypes[((ind - 1) * 2 + 1):(ind * 2), locus]
-    if (all(currentGeno == c(0, 0)) == TRUE) {
-      currentPhasedGeno <- c(1, 0, 0, 0)
-    } else if (all(currentGeno == c(0, 1)) == TRUE) {
-      currentPhasedGeno<- c(0, 1, 0, 0)
-    } else if (all(currentGeno == c(1, 0)) == TRUE) {
-      currentPhasedGeno <- c(0, 0, 1, 0)
-    } else {
-      currentPhasedGeno <- c(0, 0, 0, 1)
-    }
-    phasedGenotypes[((ind - 1) * 4 + 1):(ind * 4), 1] <- ind
-    phasedGenotypes[((ind - 1) * 4 + 1):(ind * 4), locus + 1] <- currentPhasedGeno
-  }
+# generating sequence reads
+sequence <- simulateSeqReads(genotypes, depth = 3, error = seqError)
+
+# generating pedigree
+pedigree <- getPed(basePop)
+
+# generating genotypes with errors
+genotypes_w_error <- generateGenoErr(genotypes, error = genoError)
+
+# generating genotypes with missing data
+# systematic missing 
+# low density with 100 markers (1 marker known and 9 unknowns)
+geno_missing <- matrix(rep(c(rep(1, times = 9), 0), times = nLoci / 10 * nInd), nrow = nInd, ncol = nLoci, byrow = TRUE)
+for (ind in Parents) {
+  geno_missing[as.numeric(ind), ] <- matrix(0, nrow = 1, ncol = nLoci)
 }
-
-# ----- Genotypes -----
-
-genotypes <- pullSnpGeno(pop = allPop, snpChip = "HD")
-genotypesObs <- genotypes
+genotypes_w_missing <- genotypes_w_error
 for (ind in 1:nInd) {
-  if (!ind %in% Parents) {
-    genotypesObs[ind, markersHD_without_LD] <- 9
-  }
+  genotypes_w_missing[ind, geno_missing[ind, ] == 1] <- 9
 }
-genotypesObs_w_error <- generateGenoErr(geno = genotypesObs, error = genoError)
 
-# ----- Realised allele frequency in the base population -----
-
-maf <- matrix(data = colMeans(x = genotypes[1:nIndPerGen, ]) / 2,
-              nrow = nLociAll, ncol = 1)
-summary(maf[, 1])
-hist(maf)
-
-# ----- Realised genotype error rate -----
-
-geno_error <- matrix(0, nrow = nLociAll, ncol = 1)
-for (locus in 1:nLociAll) {
+# generating genotypes error rate
+geno_error <- matrix(0, nrow = nLoci, ncol = 1)
+for (locus in 1:nLoci) {
   total <- 0
   nerrors <- 0
   for (ind in 1:nInd) {
-    if (genotypesObs_w_error[ind, locus] != 9) {
+    if (genotypes_w_missing[ind, locus] != 9) {
       total <- total + 1
-      if (genotypesObs_w_error[ind, locus] != genotypes[ind, locus]) {
+      if (genotypes_w_error[ind, locus] != genotypes[ind, locus]) {
         nerrors <- nerrors + 1
       }
     }
   }
   geno_error[locus, ] <- nerrors / total
 }
-genoError
-summary(geno_error[, 1])
 
-# ----- Sequence reads -----
+# generating sequence error rate
+seq_error <- matrix(0, nrow = nLoci, ncol = 1)
+n_ref <- matrix(0, nrow = nInd, ncol = 1)
+n_alt <- matrix(0, nrow = nInd, ncol = 1)
 
-sequenceReads <- simulateSeqReads(genotypes, depth = seqDepth, error = seqError)
-
-# ----- Realised sequence error rate -----
-
-seq_error <- matrix(data = 0, nrow = nLociAll, ncol = 1)
-for (locus in 1:nLociAll) {
-  startInd <- 1
-  endInd <- 2
-  nSeqReadError <- 0
-  nSeqReadTotal <- sum(sequenceReads[, locus])
+for (locus in 1:nLoci) {
+  p_aa <- matrix(0, nrow = nInd, ncol = 1)
+  p_AA <- matrix(0, nrow = nInd, ncol = 1)
+  genotype_locus = genotypes[, locus]
   for (ind in 1:nInd) {
-    nRef <- sequenceReads[startInd, locus]
-    nAlt <- sequenceReads[endInd, locus]
-    if (genotypes[ind, locus] == 0) {
-      nSeqReadError <- nSeqReadError + nAlt
-    } else if (genotypes[ind, locus] == 2) {
-      nSeqReadError <- nSeqReadError + nRef
+    if (genotype_locus[ind] == 0) {
+      p_aa[ind, ] <- 1
     }
-    startInd <- endInd + 1
-    endInd <- startInd + 1
+    else {
+      if (genotype_locus[ind] == 2) {
+        p_AA[ind, ] <- 1
+      }
+    }
+    n_ref[ind, ] <- sequence[(ind - 1) * 2 + 1, locus]
+    n_alt[ind, ] <- sequence[ind * 2, locus]
   }
-  seq_error[locus, 1] <- nSeqReadError / nSeqReadTotal
+  seq_error[locus, ] <- sum(n_alt * p_aa + n_ref * p_AA) / sum((n_ref + n_alt) * (p_aa + p_AA))
 }
-seqError
-summary(seq_error[, 1])
 
-# ----- Recombination events ------
+# generating minor allele frequency
+maf <- matrix(0, nrow = nInd, ncol = 1)
 
-# Needed for the calculation of segregation and the recombination rate
-# recHist[[individual]][[chromosome]][[haplotype]] 
-# column 1 gives the parental haplotype, column 2 gives the start loci
-recHist <- SP$recHist
+for (locus in 1:nLoci) {
+  count <- 0
+  for (ind in 1:nInd) {
+    count <- count + genotypes[ind, locus]
+  }
+  maf[locus, ] <- count / (nInd * 2)
+}
 
-# ----- Realised segregation ------
+# generating segregation file
+segregation <- matrix(0, nrow = nInd * 4, ncol = nLoci + 1)
+segregation[1:(4 * nInd / nGen), 2:(nLoci + 1)] <- matrix(rep(0.25, times = nLoci * 4 * nInd / nGen), nrow = 4 * nInd / nGen, ncol = nLoci)
 
-segregation <- matrix(data = 0, nrow = nInd * 4, ncol = nLociAll + 1)
-segregation[1:(4 * nIndPerGen), 1] <- rep(x = 1:nIndPerGen, each = 4)
-segregation[1:(4 * nIndPerGen), 2:(nLociAll + 1)] <- 
-  matrix(data = rep(0.25, times = nLociAll * 4 * nIndPerGen), 
-         nrow = 4 * nIndPerGen, ncol = nLociAll)
-
-for (ind in (nIndPerGen + 1):nInd) {
-  indRecHist <- recHist[[ind]][[1]]
+for (ind in ((nInd / nGen) + 1):nInd) {
+  indrecHist <- recHist[[ind]][[1]]
   
-  nMaternalComb <- nrow(indRecHist[[1]])
-  maternalPattern <- matrix(0, nrow = 4, ncol = nLociAll)
+  nMaternalComb <- nrow(indrecHist[[1]])
+  maternalPattern <- matrix(0, nrow = 4, ncol = nLoci)
   for (comb in (1:nMaternalComb)) {
-    start <- indRecHist[[1]][comb, 2]
-    if (indRecHist[[1]][comb, 1] == 1) {
+    start <- indrecHist[[1]][comb, 2]
+    if (indrecHist[[1]][comb, 1] == 1) {
       # maternal haplotype is inherited from the individual's grandmaternal
       pattern <- c(0, 1, 0, 1)
-    } else {
+    }
+    else {
       # maternal haplotype is inherited from the individual's grandpaternal
       pattern <- c(1, 0, 1, 0)
     }
-    maternalPattern[1:4, start:nLociAll] <- 
-      matrix(data = rep(pattern, times = nLociAll - start + 1), 
-             nrow = 4, ncol = nLociAll - start + 1)
+    maternalPattern[1:4, start:nLoci] <- matrix(rep(pattern, times = nLoci - start + 1), nrow = 4, ncol = nLoci - start + 1)
   }
   
-  nPaternalComb <- nrow(indRecHist[[2]])
-  paternalPattern <- matrix(0, nrow = 4, ncol = nLociAll)
+  nPaternalComb <- nrow(indrecHist[[2]])
+  paternalPattern <- matrix(0, nrow = 4, ncol = nLoci)
   for (comb in (1:nPaternalComb)) {
-    start <- indRecHist[[2]][comb, 2]
-    if (indRecHist[[2]][comb, 1] == 1) {
+    start <- indrecHist[[2]][comb, 2]
+    if (indrecHist[[2]][comb, 1] == 1) {
       # paternal haplotype is inherited from the individual's grandmaternal
       pattern <- c(0, 0, 1, 1)
-    } else {
+    }
+    else {
       # paternal haplotype is inherited from the individual's grandpaternal
       pattern <- c(1, 1, 0, 0)
     }
-    paternalPattern[1:4, start:nLociAll] <- 
-      matrix(data = rep(pattern, times = nLociAll - start + 1),
-             nrow = 4, ncol = nLociAll - start + 1)
+    paternalPattern[1:4, start:nLoci] <- matrix(rep(pattern, times = nLoci - start + 1), nrow = 4, ncol = nLoci - start + 1)
   }
   
   startRow <- 4 * (ind - 1) + 1
   endRow <- 4 * ind
-  segregation[startRow:endRow, 2:(nLociAll + 1)] <- maternalPattern * paternalPattern
+  segregation[startRow:endRow, 1] <- ind
+  segregation[startRow:endRow, 2 : (nLoci + 1)] <- maternalPattern * paternalPattern
 }
 
-# ---- TODO: Realised recombination rate ----
+# generating genotype probability file
+geno_prob <- matrix(0, nrow = nInd * 4, ncol = nLoci + 1)
+for (ind in (1:nInd)) {
+  for (locus in (1:nLoci)) {
+    currentGeno <- haplotypes[((ind - 1) * 2 + 1):(ind * 2), locus]
+    if (all(currentGeno == c(0, 0)) == TRUE) {
+      current_geno_prob <- c(1, 0, 0, 0)
+    }
+    else {
+      if (all(currentGeno == c(0, 1)) == TRUE) {
+        current_geno_prob <- c(0, 1, 0, 0)
+      }
+      else {
+        if (all(currentGeno == c(1, 0)) == TRUE) {
+          current_geno_prob <- c(0, 0, 1, 0)
+        }
+        else {
+          current_geno_prob <- c(0, 0, 0, 1)
+        }
+      }
+    }
+    geno_prob[((ind - 1) * 4 + 1):(ind * 4), 1] <- ind
+    geno_prob[((ind - 1) * 4 + 1):(ind * 4), (locus + 1)] <- matrix(current_geno_prob, nrow = 4, ncol = 1)
+  }
+}
 
-# I think this would be best done by:
-# 1) Allocating a vector of 0 for nLociAll
-# 2) Accumulating the number of recombinations per locus by parsing the recHist
-#    object - for every recombination event we bump the number for one
+# TODO: generating recombination rate
 
-# ---- Write the files to disk ----
+# write the files
+# modify the name for genotypes and haplotypes files later when calling threshold is known
+write.table(genotypes, "true-called.txt", row.names = TRUE, col.names = FALSE, quote = FALSE)
+write.table(haplotypes, "true-called_phase.txt", row.names = TRUE, col.names = FALSE, quote = FALSE)
+write.table(sequence, "seqfile.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(pedigree, "pedigree.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(geno_error, "true-genoError.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(seq_error, "true-seqError.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(maf, "true-maf.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(genotypes_w_missing, "genotypes.txt", row.names = TRUE, col.names = FALSE, quote = FALSE)
+write.table(segregation, "true-seg.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(geno_prob, "true-haps.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
-# TODO: modify the name for genotypes and haplotypes files later when calling threshold is known
 
-write.table(x = pedigree, file = "pedigree.txt", 
-            row.names = FALSE, col.names = FALSE, quote = FALSE)
-write.table(x = genotypesObs_w_error, file = "genotypes.txt", 
-            row.names = TRUE, col.names = FALSE, quote = FALSE)
-write.table(x = sequenceReads, file = "seqfile.txt", 
-            row.names = FALSE, col.names = FALSE, quote = FALSE)
+# Map files for peeling
 
-write.table(x = haplotypes, file = "true-called_phase.txt", 
-            row.names = TRUE, col.names = FALSE, quote = FALSE)
-write.table(x = phasedGenotypes, file = "true-haps.txt", 
-            row.names = FALSE, col.names = FALSE, quote = FALSE)
-write.table(x = genotypes, file = "true-called.txt", 
-            row.names = TRUE, col.names = FALSE, quote = FALSE)
+nLoci = ncol(genotypes)
+values = data.frame(1, paste0("1-", 1:nLoci), 1:nLoci)
+write.table(values, "map.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
-write.table(x = geno_error, file = "true-genoError.txt", 
-            row.names = FALSE, col.names = FALSE, quote = FALSE)
-write.table(x = seq_error, file = "true-seqError.txt", 
-            row.names = FALSE, col.names = FALSE, quote = FALSE)
-write.table(x = maf, file = "true-maf.txt", 
-            row.names = FALSE, col.names = FALSE, quote = FALSE)
-write.table(x = segregation, "true-seg.txt", 
-            row.names = FALSE, col.names = FALSE, quote = FALSE)
+subset = floor(seq(1, nLoci, length.out = 200))
+subsetValues = values[subset,]
+write.table(subsetValues, "segmap.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
-# TODO: make this code general in terms of the number of chromosomes (see at the top of the script)
-values <- data.frame(1, paste0("1-", 1:nLociAll), 1:nLociAll)
-write.table(x = values, file = "map.txt",
-            row.names = FALSE, col.names = FALSE, quote = FALSE)
-
-# TODO: convert 200 to an expression so this code will be more general
-subset <- floor(seq(1, nLociAll, length.out = 200))
-subsetValues <- values[subset,]
-write.table(x = subsetValues, file = "segmap.txt",
-            row.names = FALSE, col.names = FALSE, quote = FALSE)
