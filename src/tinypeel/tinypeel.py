bd02d43518e85c90de3454243724e7596fcea13a
AprilYUZhang <zaprily98@gmail.com>
2023-11-16 14:35:05 +0000

    fix minor probelms according to precommit

--- src/tinypeel/tinypeel.py
+++ src/tinypeel/tinypeel.py
@@ -31,6 +31,7 @@ def runPeelingCycles(pedigree, peelingInfo, args, singleLocusMode=False):
         if args.est_geno_error_prob or args.est_seq_error_prob:
             PeelingUpdates.updatePenetrance(pedigree, peelingInfo)
 
+
 def peelingCycle(pedigree, peelingInfo, args, singleLocusMode=False):
     nWorkers = args.maxthreads
 
@@ -240,30 +241,75 @@ def get_probability_options():
 
 def get_input_options():
     parse_dictionary = dict()
-    parse_dictionary["bfile"] = lambda parser: parser.add_argument('-plink_file', default=None, required=False, type=str,
-                                                                   nargs="*",
-                                                                   help='A file in plink (binary) format. Only stable on Linux).')
-    parse_dictionary["genotypes"] = lambda parser: parser.add_argument('-geno_file', default=None, required=False,
-                                                                       type=str, nargs="*",
-                                                                       help='A file in AlphaGenes format.')
-    parse_dictionary["reference"] = lambda parser: parser.add_argument('-reference', default=None, required=False,
-                                                                       type=str, nargs="*",
-                                                                       help='A haplotype reference panel in AlphaGenes format.')
-    parse_dictionary["seqfile"] = lambda parser: parser.add_argument('-seq_file', default=None, required=False, type=str,
-                                                                     nargs="*", help='A sequence data file.')
-    parse_dictionary["pedigree"] = lambda parser: parser.add_argument('-ped_file', default=None, required=False,
-                                                                      type=str, nargs="*",
-                                                                      help='A pedigree file in AlphaGenes format.')
-    parse_dictionary["phasefile"] = lambda parser: parser.add_argument('-phasefile', default=None, required=False,
-                                                                       type=str, nargs="*",
-                                                                       help='A phase file in AlphaGenes format.')
-    parse_dictionary["startsnp"] = lambda parser: parser.add_argument('-startsnp', default=None, required=False,
-                                                                      type=int,
-                                                                      help="The first marker to consider. The first marker in the file is marker '1'. Default: 1.")
-    parse_dictionary["stopsnp"] = lambda parser: parser.add_argument('-stopsnp', default=None, required=False, type=int,
-                                                                     help='The last marker to consider. Default: all markers considered.')
-    parse_dictionary["seed"] = lambda parser: parser.add_argument('-seed', default=None, required=False, type=int,
-                                                                  help='A random seed to use for debugging.')
+    parse_dictionary["bfile"] = lambda parser: parser.add_argument(
+        "-plink_file",
+        default=None,
+        required=False,
+        type=str,
+        nargs="*",
+        help="A file in plink (binary) format. Only stable on Linux).",
+    )
+    parse_dictionary["genotypes"] = lambda parser: parser.add_argument(
+        "-geno_file",
+        default=None,
+        required=False,
+        type=str,
+        nargs="*",
+        help="A file in AlphaGenes format.",
+    )
+    parse_dictionary["reference"] = lambda parser: parser.add_argument(
+        "-reference",
+        default=None,
+        required=False,
+        type=str,
+        nargs="*",
+        help="A haplotype reference panel in AlphaGenes format.",
+    )
+    parse_dictionary["seqfile"] = lambda parser: parser.add_argument(
+        "-seq_file",
+        default=None,
+        required=False,
+        type=str,
+        nargs="*",
+        help="A sequence data file.",
+    )
+    parse_dictionary["pedigree"] = lambda parser: parser.add_argument(
+        "-ped_file",
+        default=None,
+        required=False,
+        type=str,
+        nargs="*",
+        help="A pedigree file in AlphaGenes format.",
+    )
+    parse_dictionary["phasefile"] = lambda parser: parser.add_argument(
+        "-phasefile",
+        default=None,
+        required=False,
+        type=str,
+        nargs="*",
+        help="A phase file in AlphaGenes format.",
+    )
+    parse_dictionary["startsnp"] = lambda parser: parser.add_argument(
+        "-startsnp",
+        default=None,
+        required=False,
+        type=int,
+        help="The first marker to consider. The first marker in the file is marker '1'. Default: 1.",
+    )
+    parse_dictionary["stopsnp"] = lambda parser: parser.add_argument(
+        "-stopsnp",
+        default=None,
+        required=False,
+        type=int,
+        help="The last marker to consider. Default: all markers considered.",
+    )
+    parse_dictionary["seed"] = lambda parser: parser.add_argument(
+        "-seed",
+        default=None,
+        required=False,
+        type=int,
+        help="A random seed to use for debugging.",
+    )
 
     return parse_dictionary
 
