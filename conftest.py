d4309f6d7b0b129c123fef8de274bd2558aeefc1
xinger <s2186420@ed.ac.uk>
2023-11-30 20:47:57 +0000

    Update test for option phasefile and update test report

--- conftest.py
+++ conftest.py
@@ -50,8 +50,8 @@ def pytest_runtest_makereport():
         with open("tests/accuracy_tests/accu_report.txt", "a") as file:
             for i in range(num_file):
                 assessed_file = accu[i * 3 + 1].split()[-1]
-                file.write(name + "|" + assessed_file + "$" + accu[i * 3 + 2] + "\n")
-                file.write(name + "|" + assessed_file + "$" + accu[i * 3 + 3] + "\n")
+                file.write(name + " " + assessed_file + " " + accu[i * 3 + 2] + "\n")
+                file.write(name + " " + assessed_file + " " + accu[i * 3 + 3] + "\n")
 
 
 @pytest.hookimpl()
@@ -75,7 +75,9 @@ def pytest_terminal_summary(terminalreporter):
         "seg_prob",
     ]
     columns = (
-        "Type",
+        "Test Name",
+        "File Name",
+        "Accu Type",
         "Population Accu",
         "Gen1 Accu",
         "Gen2 Accu",
@@ -83,20 +85,18 @@ def pytest_terminal_summary(terminalreporter):
         "Gen4 Accu",
         "Gen5 Accu",
     )
-    dt = {"names": columns, "formats": ("S76", "f4", "f4", "f4", "f4", "f4", "f4")}
-    accu = np.loadtxt("tests/accuracy_tests/accu_report.txt", dtype=dt)
+    dt = {"names": columns, "formats": ("U76", "U25", "U25") + ("f4",) * (nGen + 1)}
+    accu = np.loadtxt("tests/accuracy_tests/accu_report.txt", encoding=None, dtype=dt)
 
     mkr_accu = list(
         filter(
-            lambda x: (x["Type"].decode("UTF-8").split("$")[1] == "Marker_accuracies"),
+            lambda x: x["Accu Type"] == "Marker_accuracies",
             accu,
         )
     )
     ind_accu = list(
         filter(
-            lambda x: (
-                x["Type"].decode("UTF-8").split("$")[1] == "Individual_accuracies"
-            ),
+            lambda x: x["Accu Type"] == "Individual_accuracies",
             accu,
         )
     )
@@ -106,49 +106,65 @@ def pytest_terminal_summary(terminalreporter):
     for file_type in file_types:
         mkr_accu_file[file_type] = list(
             filter(
-                lambda x: (
-                    x["Type"].decode("UTF-8").split("|")[1].split("$")[0] == file_type
-                ),
+                lambda x: x["File Name"] == file_type,
                 mkr_accu,
             )
         )
         ind_accu_file[file_type] = list(
             filter(
-                lambda x: (
-                    x["Type"].decode("UTF-8").split("|")[1].split("$")[0] == file_type
-                ),
+                lambda x: x["File Name"] == file_type,
                 ind_accu,
             )
         )
+
+    test_names = list(
+        map(
+            lambda x: x["Test Name"],
+            filter(lambda x: x["File Name"] == file_types[0], mkr_accu),
+        )
+    )
+    test_nums = {}
+    count = 0
+    for test_name in test_names:
+        count += 1
+        test_nums[test_name] = count
 
     sorted_mkr_accu_file = {}
     sorted_ind_accu_file = {}
     for file_type in file_types:
         sorted_mkr_accu_file[file_type] = sorted(
             mkr_accu_file[file_type],
-            key=operator.itemgetter(*columns[1:]),
+            key=operator.itemgetter(*columns[3:]),
             reverse=True,
         )
         sorted_ind_accu_file[file_type] = sorted(
             ind_accu_file[file_type],
-            key=operator.itemgetter(*columns[1:]),
+            key=operator.itemgetter(*columns[3:]),
             reverse=True,
         )
 
-    format_first_row = "{:<35} " + "{:<20} " * (nGen + 1)
-    format_row = "{:<35} " + "{:<20.3f} " * (nGen + 1)
+    format_first_row = "{:<20} " * (nGen + 2) + "{:<35} "
+    format_row = "{:<20.0f} " + "{:<20.3f} " * (nGen + 1) + "{:<35} "
+    out_columns = (
+        "Test Num",
+        "Population Accu",
+        "Gen1 Accu",
+        "Gen2 Accu",
+        "Gen3 Accu",
+        "Gen4 Accu",
+        "Gen5 Accu",
+        "Test Name",
+    )
 
     def write_report(accu_type, sorted=False):
         if not sorted:
-            terminalreporter.write_sep("=", accu_type + " Accuaracy")
+            terminalreporter.write_sep("=", accu_type + " Accuracy")
             if accu_type == "Marker":
                 reports = mkr_accu_file
             else:
                 reports = ind_accu_file
         else:
-            terminalreporter.write_sep(
-                "=", accu_type + " Accuaracy (Order by accuracy)"
-            )
+            terminalreporter.write_sep("=", accu_type + " Accuracy (Order by accuracy)")
             if accu_type == "Marker":
                 reports = sorted_mkr_accu_file
             else:
@@ -156,17 +172,18 @@ def pytest_terminal_summary(terminalreporter):
 
         for file_type in file_types:
             terminalreporter.write_sep("-", file_type)
-            terminalreporter.write_line(format_first_row.format(*columns))
+            terminalreporter.write_line(format_first_row.format(*out_columns))
             for test in reports[file_type]:
                 terminalreporter.write_line(
                     format_row.format(
-                        test["Type"].decode("UTF-8").split("|")[0],
+                        test_nums[test["Test Name"]],
                         test["Population Accu"],
                         test["Gen1 Accu"],
                         test["Gen2 Accu"],
                         test["Gen3 Accu"],
                         test["Gen4 Accu"],
                         test["Gen5 Accu"],
+                        test["Test Name"],
                     )
                 )
 
